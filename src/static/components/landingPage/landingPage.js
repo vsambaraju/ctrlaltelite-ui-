1b8d35b078caa6de1454368e49ce38c8bebcdfd2
Amy Butz <c-amy.butz@charter.com>
2018-10-13 09:57:12 -0500

    Creates Login form component

--- src/static/components/landingPage/landingPage.js
+++ src/static/components/landingPage/landingPage.js
@@ -1,34 +1,53 @@
 import React from "react";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
+import Paper from "@material-ui/core/Paper";
 import Avatar from "@material-ui/core/Avatar";
 import {withStyles} from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import PermPhoneMsg from "@material-ui/icons/PermPhoneMsg";
-import Typography from "@material-ui/core/Typography";
 import ColorPalette from "../ui/constants/colorPalette";
+import Login from "./login";
 
-const styles = {
+const styles = theme => ({
     avatar: {
         margin: "10px",
         color: "white",
+        width: "90px",
+        height: "90px",
         backgroundColor: ColorPalette.PRIMARY,
+    },
+    layout: {
+        width: 'auto',
+        display: 'block', // Fix IE11 issue.
+        marginLeft: theme.spacing.unit * 3,
+        marginRight: theme.spacing.unit * 3,
+        [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+            width: 400,
+            marginLeft: 'auto',
+            marginRight: 'auto',
+        },
+    },
+    paper: {
+        marginTop: theme.spacing.unit * 8,
+        display: 'flex',
+        flexFlow: 'column nowrap',
+        alignItems: 'center',
+        padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+    },
+    submit: {
+        marginTop: theme.spacing.unit * 3,
+        color: ColorPalette.PRIMARY
     }
-};
+});
 
 const LandingPage = ({classes}) => (
-    <div>
-            <Card >
-                <CardContent>
-                    <Typography gutterBottom variant="headline">
-                        Hello
-                    </Typography>
-                    <Avatar className={classes.avatar}>
-                        <PermPhoneMsg />
-                    </Avatar>
-                </CardContent>
-            </Card>
-    </div>
+    <main className={classes.layout}>
+        <Paper className={classes.paper}>
+            <Avatar className={classes.avatar}>
+                <PermPhoneMsg />
+            </Avatar>
+            <Login/>
+        </Paper>
+    </main>
 );
 
-export default withStyles(styles)(LandingPage);
+export default withStyles(styles, {withTheme: true})(LandingPage);
